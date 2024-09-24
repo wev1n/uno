7cdc3a705d44ae17aebe81f85d8497354a3b92d1
wevin <kevin.warme@gmail.com>
2024-09-24 20:13:11 +0200

    Relocate components used on the game-board to separate folder

---
+++ components/game-board/discard-pile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DiscardPile = () => {
+  return <div>DiscardPile</div>;
+};
+
+export default DiscardPile;
---
+++ components/game-board/uno-card.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UnoCard = () => {
+  return <div>UnoCard</div>;
+};
+
+export default UnoCard;
